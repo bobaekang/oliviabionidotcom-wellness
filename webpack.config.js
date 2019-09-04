f593e45e061d1a6f228a1704e29d6dc9fc13924f
bobaekang <bobaekang@uchicago.edu>
2019-09-03 20:10:58 -0500

    chore: set up webpack config

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  devServer: {
+      contentBase: './public'
+  } 
+}

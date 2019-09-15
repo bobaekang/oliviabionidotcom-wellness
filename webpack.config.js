0b2349d53c449f003202138ae2a802fb8236261b
bobaekang <bobaekang@uchicago.edu>
2019-09-15 14:25:21 -0500

    chore: set up HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
@@ -50,5 +51,11 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [ 
+    new HtmlWebpackPlugin({
+        template: path.join(__dirname, '/src/index.html'),
+        inject: 'body'
+    })
+  ]
 }

7683b78b23d26c68750e1b927bb9cde15816e235
bobaekang <bobaekang@uchicago.edu>
2019-10-20 11:06:38 -0500

    feat: toggle fixed header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 require('./style/index.css');
+require('./script/toggleHeader')
---
+++ src/script/toggleHeader.js
@@ -0,0 +1,37 @@
+/* Credit: https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c */
+
+'use strict';
+
+var didScroll;
+var lastScrollTop = 0;
+
+window.addEventListener('scroll', function(x) {
+  didScroll = true;
+});
+
+setInterval(function() {
+  var scrollTop = window.scrollY;
+  
+  if (didScroll) {
+    if (Math.abs(lastScrollTop - scrollTop) > 10) {
+      toggleHeader(scrollTop);
+      lastScrollTop = scrollTop;
+    }
+    
+    didScroll = false;
+  }
+}, 250);
+
+function toggleHeader (scrollTop) {
+  var el = document.querySelector('.header-fixed')
+  
+  if (scrollTop > lastScrollTop && scrollTop > el.offsetHeight) {
+      // Scroll Down
+      el.classList.remove('header-hide');
+  } else {
+      // Scroll Up
+      if (scrollTop + window.innerHeight < document.body.scrollHeight) {
+        el.classList.add('header-hide');
+      }
+  }
+}
